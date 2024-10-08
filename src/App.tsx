1f1d7f921a8fdd24466e17819a91992dcea506f7
Nugar <cladeadenugar@gmail.com>
2024-10-08 17:13:51 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { motion } from "framer-motion";
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import { duration } from "@mui/material";
 const App = () => {
   return (
     <div>
       <section>
+        <div className="course">
+          <motion.div
+            className="box"
+            animate={{ opacity: 0 }}
+            transition={{ duration: 2, delay: 2 }}
+          ></motion.div>
+        </div>
         <Navbar />
       </section>
       <section>a</section>
